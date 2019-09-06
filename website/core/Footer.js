5f7179caf5e8d4ebf41e64fa035a597cd14db36d
tallyb <tally.barak@gmail.com>
2019-09-06 09:09:20 +0300

    adding ga and removing footer icon

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -31,7 +31,7 @@ class Footer extends React.Component {
     return (
       <footer className="nav-footer" id="footer">
         <section className="sitemap">
-          <a href={this.props.config.baseUrl} className="nav-home">
+          <a href='https://bit.dev' className="nav-home">
             {this.props.config.footerIcon && (
               <img
                 src={this.props.config.baseUrl + this.props.config.footerIcon}
@@ -90,18 +90,6 @@ class Footer extends React.Component {
           </div>
         </section>
 
-        <a
-          href="https://bit.dev/"
-          target="_blank"
-          rel="noreferrer noopener"
-          className="fbOpenSource">
-          <img
-            src={`${this.props.config.baseUrl}img/logo.svg`}
-            alt="Bit Open Source"
-            width="170"
-            height="45"
-          />
-        </a>
         <section className="copyright">{this.props.config.copyright}</section>
       </footer>
     );
