c8253b929f0910d4cd8aa57a1c08a3e9b3b4ccc8
Eden <49904302+giteden@users.noreply.github.com>
2021-02-16 22:39:31 +0200

    Delete .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/eden/Documents/Projects/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/eden/Documents/Projects/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/eden/Documents/Projects/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/eden/Documents/Projects/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/eden/Documents/Projects/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/eden/Documents/Projects/docs/src/css/custom.css"),
-];
