bcf62a7e75b9a4f56a04d11c31d8d2cd6dd64224
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2021-03-30 20:21:25 +0200

    fix: slack link

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -46,7 +46,7 @@ class Footer extends React.Component {
           </div>
           <div>
             <h5>Community</h5>
-            <a href="https://join.slack.com/t/bit-dev-community/shared_invite/enQtNzM2NzQ3MTQzMTg3LWI2YmFmZjQwMTkxNmFmNTVkYzU2MGI2YjgwMmJlZDdkNWVhOGIzZDFlYjg4MGRmOTM4ODAxNTIxMTMwNWVhMzg">
+            <a href="https://join.slack.com/t/bit-dev-community/shared_invite/zt-o2tim18y-UzwOCFdTafmFKEqm2tXE4w">
               Slack
             </a>
             <a
