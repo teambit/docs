a699b61eac3980f32a13db346f9a7537d4ae8435
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-04-30 08:29:39 +0200

    feat: youtube videos

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -272,9 +272,9 @@ module.exports = {
             'https://github.com/facebook/docusaurus/edit/master/website/blog/'
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css')
+          customCss: [require.resolve('./src/css/custom.css')]
         }
       }
     ]
   ]
-}
+};
