ba7e7e06e51af13efc597af6e4b61b19bac89ee2
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-08-12 21:42:02 +0200

    set collapsable to false

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -315,8 +315,8 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          sidebarCollapsible: true,
-          sidebarCollapsed: true,
+          sidebarCollapsible: false,
+          sidebarCollapsed: false,
           // Please change this to your repo.
           editUrl: 'https://github.com/teambit/docs/edit/new-harmony-docs',
           routeBasePath: '/',
